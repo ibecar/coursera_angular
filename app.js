657f885c213ceaff1546b5c486afa29e8788770d
Ivan Becar <ibecar@gmail.com>
2017-01-26 20:59:53 +0100

    finish

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-( function() {
-  angular.module('week1app')
-  .controller("")
-});
+(function() {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller("LunchCheckController", LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+
+    $scope.checkMe = function() {
+      if ($scope.input == null || $scope.input == '') {
+        $scope.errorEmptyData = true;
+        $scope.error = false;
+        $scope.ok = false;
+      } else {
+        var items = $scope.input.split(",");
+        var count = countItems(items);
+        $scope.error = count > 3
+        $scope.ok = !$scope.error;
+        $scope.errorEmptyData = false;
+      }
+    };
+
+    function countItems(items) {
+      var count = 0;
+      for (var key in items ) {
+        if (items[key].trim() !== '') {
+          count++;
+        }
+      }
+      return count;
+    };
+  }
+})();
